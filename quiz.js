9d108548f99aff77b0e5b007a7f859281b238c14
Surya Saputra <17169567+suryasaputra2016@users.noreply.github.com>
2024-10-22 21:38:21 +0700

    add quiz.js

---
+++ quiz.js
@@ -0,0 +1,39 @@
+let quizes = [
+    {
+        imagePath: "",
+        questionText: "A small ball is thrown with an initial velocity of 50 m/s making an angle of 37 degree with horizontal line. Assume the gravitational acceleration is 10 m/s^2 downward.\nHow far would it lands from its initial position?",
+        optionList: [
+            "120 meters",
+            "180 meters",
+            "240 meters",
+            "480 meters",
+        ],
+        answerKey: 2,
+        answerChoice: 5,
+    },
+    {
+        imagePath: "",
+        questionText: "A small block of mass 2 kg is pushed with two oposing forces of 5 Newton to the right and 3 Newton to the left along a smooth horizontal plane.\nWhat is the block's acceleration?",
+        optionList: [
+            "1 m/s^2",
+            "2 m/s^2",
+            "4 m/s^2",
+            "8 m/s^2",
+        ],
+        answerKey: 0,
+        answerChoice: 5,
+    },
+    {
+        imagePath: "",
+        questionText: "Two spherical charges are initially held apart. They repel each other with the force of 108 Newton.\nWhat would be the forces between them if their distance is increased to 3 times its initial value?",
+        optionList: [
+            "12 N",
+            "36 N",
+            "324 N",
+            "972 N",
+        ],
+        answerKey: 0,
+        answerChoice: 5,
+    },
+
+];
